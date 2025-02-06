0d3196ab7e450c2f3bd983c1319887d564f95733
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-06 10:05:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("subro selon Server is ready...");
+});
+
+app.listen(port, () => {
+  console.log(`subro selon is situated in dhaka ${port}`);
+});
